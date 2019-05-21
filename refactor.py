e923d9b9fde4e1311c1a288516e122ce4810afd2
indecipherable <atrickof.thelight@gmail.com>
2019-05-21 05:26:04 +0000

    Added logic to refactor.py
    refactor.py now accepts user input for
    [projectname] - 1905202225

--- refactor.py
+++ refactor.py
@@ -2,3 +2,34 @@
 # and refactor [barebones] to [projectname]
 
 import os
+
+# this refactors project to project name
+def refac(final_name):
+  print(final_name)
+
+# this confirms user input for project name
+def get_in(this_project_name):
+  this_y_n = raw_input("> ")
+  if this_y_n.lower() == 'y':
+    print("OK, got 'y'")
+    refac(this_project_name)
+  elif this_y_n.lower() == 'n':
+    print("OK, got 'n'")
+    ref_to()
+  else:
+    print("Nope")
+    print("Is " + this_project_name + " right? [y/n]:")
+    get_in(this_project_name)
+
+def do_next():
+  print("Ok, next!!")
+
+# this accepts user input for project name
+def ref_to():
+  print("This project name is: ")
+  this_project=raw_input("> ")
+  print("I got: '" + this_project + \
+          "'  Is that right? [y/n]:")
+  get_in(this_project)
+
+ref_to()
